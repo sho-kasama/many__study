03190ca982e52e3f31a7b5d6439966b0e37b74ae
sho-kasama <sho-kasama@trunk.fm>
2019-04-18 21:16:31 +0900

    ref #15 add: vue-router page

---
+++ six-SPA-page/src/components/page2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>ここはページ２です。</p>
+    <router-link to="/">Go to page1</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- six-SPA-page/src/router/index.js
+++ six-SPA-page/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import page1 from '@/components/page1'
+import page2 from '@/components/page2'
+import page3 from '@/components/page3'
 
 Vue.use(Router)
 
@@ -8,8 +11,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: page1
+    },
+    {
+      path: '/page2',
+      component: page2
+    },
+    {
+      path: '/page3',
+      component: page3
     }
+
+
   ]
 })
