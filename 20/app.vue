7ebfa6c37b5c63c3b1b704337173eb6669334783
sho-kasama <sho-kasama@trunk.fm>
2019-07-06 23:50:46 +0900

    vue

---
+++ 20/app.vue
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        counter: 0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        increase: function () {
+            this.counter++;
+        },
+        updateCordinates: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        }
+    }
+});
---
+++ Vue.tutorial/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        mosterHealth: 100,
+        // 実行中のゲームがないので最初はfalseに設定してる
+        gameIsRunninig: false
+    }
+
+
+});
