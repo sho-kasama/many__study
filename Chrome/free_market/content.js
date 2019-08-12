c0c9ee85a37b7211fc476a8f40dc53ccff2a96b5
sho-kasama <sho-kasama@trunk.fm>
2019-08-12 19:54:30 +0900

    add: chrome

---
+++ Chrome/free_market/content.js
@@ -0,0 +1,27 @@
+// Webページ上のDOM操作をJavaScriptで行うファイル
+
+// ページ上のDOMを取得
+const $descriptionForm = document.forms[0].description
+
+// テンプレート選択セレクトボックスを生成
+
+const $templateSelectbox = document.createElement('select')
+$templateSelectbox.className = 'form-control form-control-sm'
+$templateSelectbox.innerHTML = `<option value="">挿入するテンプレートを選択</option>`
+
+// テンプレート編集ボタンを生成
+const $templateButton = document.createElement('button')
+$templateButton.className = 'pxm-templateButton'
+$templateButton.setAttribute('type', 'button')
+$templateButton.innerHTML = `<img src="${chrome.extension.getURL(
+  'images/edit.svg'
+)}" height="20">`
+
+// テンプレート編集UI（セレクトボックス＋編集ボタン）を生成
+const $templateUi = document.createElement('div')
+$templateUi.className = 'pxm-templateUi'
+$templateUi.appendChild($templateSelectbox)
+$templateUi.appendChild($templateButton)
+
+// 「商品の説明」テキストエリアの直前にテンプレート編集UIを挿入
+$descriptionForm.before($templateUi)
